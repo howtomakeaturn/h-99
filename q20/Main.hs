44b4ef19c15638ffdf6c4bb94d7f0c4e0a459aef
howtomakeaturn <howtomakeaturn@hotmail.com>
2018-02-18 09:50:45 +0800

    more q20

--- q20/Main.hs
+++ q20/Main.hs
@@ -2,7 +2,16 @@ module Main where
 
 removeAt :: Int -> [a] -> (a, [a])
 
+-- original thought
+{-|
 removeAt n xs = (element,after)
     where
         element = last (take n xs)
         after = (take (n - 1) xs) ++ (drop n xs)
+|-}
+
+-- more recursive solution
+removeAt 1 (x:xs) = (x, xs)
+removeAt n (x:xs) = (y, x:zs)
+    where
+        (y, zs) = removeAt (n - 1) (xs)
