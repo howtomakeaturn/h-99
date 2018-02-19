fc4b6160bc778642f8e9e921ffb4444fada04fb4
howtomakeaturn <howtomakeaturn@hotmail.com>
2018-02-19 12:45:25 +0800

    add type signature

--- q23/Main.hs
+++ q23/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import System.Random
 
+rnd_select :: [Char] -> Int -> StdGen -> [Char]
 rnd_select xs 0 gen = ""
 rnd_select xs n gen = selected : rnd_select rest (n-1) gen
     where
@@ -9,4 +10,5 @@ rnd_select xs n gen = selected : rnd_select rest (n-1) gen
         selected = last (take randomIndex xs)
         rest = take (randomIndex-1) xs ++ drop randomIndex xs
 
+gen_random :: Int -> Int -> StdGen -> (Int,StdGen)
 gen_random n m gen = randomR (n,m) gen :: (Int, StdGen)
